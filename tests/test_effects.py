9fa8c0770021fc4ff67b625c948464b0e3e28758
Richard Morris <richard@khiron.com>
2025-08-07 17:37:03 +1000

    Add pytest and coverage to dependencies and create a dummy test file

---
+++ tests/test_effects.py
@@ -0,0 +1,13 @@
+import pytest
+from Qubitrix.sounds import Effects
+
+def test_singleton():
+    e1 = Effects()
+    e2 = Effects()
+    assert e1 is e2
+
+def test_lazy_loading():
+    effects = Effects()
+    # This assumes you have a sound file named "sonic_drop.wav" in the sounds folder
+    effect = effects._load_sound("sonic_drop")
+    assert effect.name == "sonic_drop"
