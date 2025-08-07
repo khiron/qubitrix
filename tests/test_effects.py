3f58d9f7a27141c92f6480b6fc10017d0b3dc976
Richard Morris <richard@khiron.com>
2025-08-07 19:29:35 +1000

    fix a bug identified by test_lazy_loading that you can't create a Sound if the mixer is not initialised

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -7,6 +7,9 @@ def test_singleton():
     assert e1 is e2
 
 def test_lazy_loading():
+    import pygame
+    pygame.mixer.init()
+    
     effects = Effects()
     # This assumes you have a sound file named "sonic_drop.wav" in the sounds folder
     effect = effects._load_sound("sonic_drop")
